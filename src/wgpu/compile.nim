da31ba67427dd198f462f89c43f2975c02a50a56
sOkam <7308253+heysokam@users.noreply.github.com>
2026-04-11 05:00:46 +0200

    fix: Runtime error caused by missing flags for surface creation
    Hits a different error after, but the previous one is solved :noted:

--- src/wgpu/compile.nim
+++ src/wgpu/compile.nim
@@ -7,7 +7,48 @@ const thisDir    = currentSourcePath().parentDir()
 const includeDir = thisDir/"C"/"wgvk"/"include"
 const srcDir     = thisDir/"C"/"wgvk"/"src"
 
+#_____________________________
+# Default flags
 {.passL:"-lvulkan".}
 {.passC:"-I"&includeDir.}
+
+
+#_____________________________
+# Platform surfaces
+when defined(windows):
+  {.passC: "-DSUPPORT_WIN32_SURFACE=1".}
+  {.passL: "-luser32 -lgdi32".}
+elif defined(macosx):
+  {.passC: "-DSUPPORT_METAL_SURFACE=1".}
+  {.passL: "-framework Cocoa -framework Metal -framework QuartzCore".}
+elif defined(linux):
+  when defined(wayland):
+    {.passC: "-DSUPPORT_WAYLAND_SURFACE=1".}
+    {.passL: "-lwayland-client".}
+  else:
+    {.passC: "-DSUPPORT_XLIB_SURFACE=1".}
+    {.passL: "-lX11".}
+elif defined(android):
+  {.passC: "-DSUPPORT_ANDROID_SURFACE=1".}
+#_____________________________
+# Vulkan 1.3 features
+when defined(wgvkDynamicRendering):
+  {.passC: "-DVULKAN_USE_DYNAMIC_RENDERING=1".}
+when defined(wgvkRaytracing):
+  {.passC: "-DVULKAN_ENABLE_RAYTRACING=1".}
+#_____________________________
+# Optional features
+when defined(wgvkWGSL):
+  {.passC: "-DSUPPORT_WGSL=1".}
+when defined(wgvkGLSL):
+  {.passC: "-DSUPPORT_GLSL=1".}
+when defined(wgvkVMA):
+  {.passC: "-DUSE_VMA_ALLOCATOR=1".}
+when defined(wgvkDRM):
+  {.passC: "-DSUPPORT_DRM_SURFACE=1".}
+
+
+#_____________________________
+# Compile wgvk
 {.compile:srcDir/"wgvk.c".}
 
